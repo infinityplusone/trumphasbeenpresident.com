c17d20eca0b77289f5ad0842396b7a77e899ffa6
infinityplusone <jonathan@infinityplusone.com>
2017-03-06 18:21:21 -0500

    custom adjectives

--- common/thbp/thbp.js
+++ common/thbp/thbp.js
@@ -37,10 +37,11 @@
     second: 0
   };
   var units = Object.keys(time);
-  var counter = document.getElementById('counter');
+  var $body = $('body');
 
   // Handlebars stuff
-  var tmpl = Handlebars.compile(document.getElementById('template-counter').innerHTML);
+  var tmplCounter = Handlebars.compile($('#template-counter').html());
+  var tmplPercent = Handlebars.compile($('#template-percent').html());
 
   Handlebars.registerHelper('pluralize', function(val, str) {
     return str + (val !== "1" ? 's' : '');
@@ -61,26 +62,38 @@
         from = from.add(diff, k);
       }
     });
-    counter.innerHTML = tmpl({
-      counter: time,
+
+    $body.find('.counter').replaceWith(tmplCounter({
+      counter: time
+    }));
+    $body.find('.percent').replaceWith(tmplPercent({
       percent: percent.toFixed(1),
       adj: adj
-    });
+    }));
   } // countUp
 
   var clock = setInterval(countUp, 1000);
 
-  document.body.onclick = function() {
-    if(clock) {
-      clearInterval(clock);
-      clock = null;
-      // console.log('stopping!');
-    }
-    else {
-      clock = setInterval(countUp, 1000);
-      // console.log('starting!');
-    }
-    
-  };
+  $body
+    // .on('mouseenter', function(e) {
+    //   var p = $('.adjective').position();
+    //   // $('#new-adjectives').css({
+    //   //   top: p.top + 120
+    //   // });
+    // })
+    .on('submit', 'form', function(e) {
+      this.action = [$('#domain').val() + $('#new-adjectives').val().replace(/\W+/gim, '/')].join('/');
+      
+    })
+    .on('click', function() {
+      $body.toggleClass('paused');
+      if(clock) {
+        clearInterval(clock);
+        clock = null;
+      }
+      else {
+        clock = setInterval(countUp, 1000);
+      }
+    });
 
 })();
