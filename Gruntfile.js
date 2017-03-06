0160a24fb1b3ba3b7c6ae13353d740eff71f69c8
infinityplusone <jonathan@infinityplusone.com>
2017-03-06 18:21:33 -0500

    v0.4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
  * Gruntfile
  *
  * Author(s):  Jonathan "Yoni" Knoll
- * Version:    0.3.0
+ * Version:    0.4.0
  * Date:       2017-03-06
  *
  */
--- common/thbp/thbp.js
+++ common/thbp/thbp.js
@@ -3,7 +3,7 @@
  * Name: index/index.js
  * Author(s): infinityplusone
 
- * Version:    0.3.0
+ * Version:    0.4.0
  * Last Modified: 2017-03-06
  *
  * Notes: 
